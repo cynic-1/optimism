0fb02824fd04ebfb4b54ef3b960faeddfdd4a59d
tre <tremarkley@gmail.com>
2023-09-12 10:47:10 -0700

    Deployment script for contracts-periphery

---
+++ packages/contracts-bedrock/src/periphery/scripts/DeployPeriphey.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { Deployer } from "../../../scripts/Deployer.sol";
+
+/// @title DeployPeriphery
+/// @notice Script used to deploy periphery contracts.
+contract DeployPeriphery is Deployer {
+
+}
